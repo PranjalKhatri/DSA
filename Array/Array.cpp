a4da7a35d220644d4b8f93da24c47d2fc5af5840
Pranjal-pc <pranjalkhatri950@gmail.com>
2023-07-18 14:39:03 +0530

    Added basic array functionality

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -3,57 +3,94 @@
 using namespace std;
 
 template <class T>
-class Array{
+class Array
+{
 private:
     T *arr;
+
 public:
     int total_size;
     int used_size;
 
     Array();
-    Array(int tsize,int usize){create(tsize,usize);}
+    Array(int tsize, int usize) { create(tsize, usize); }
 
-    void create(int tsize,int usize)
+    void create(int tsize, int usize)
     {
         total_size = tsize;
         used_size = usize;
         arr = new T[tsize];
     }
 
-    int size(){
+    int size()
+    {
         return this->used_size;
     }
 
-    void show(){
+    void show()
+    {
         for (int i = 0; i < used_size; i++)
         {
-            cout<<arr[i]<<endl;
+            cout << arr[i] << endl;
         }
-        
     }
 
-    void destroy(){
-        delete [] arr;
-    }
-    ~Array(){
-        delete [] arr;
+    void destroy()
+    {
+        delete[] arr;
     }
     
-    T &operator[](int i){
+    ~Array()
+    {
+        delete[] arr;
+    }
+
+    T &operator[](int i)
+    {
         return (arr[i]);
+    }
+
+    void insert_at(T ele, int pos)
+    {
+        if (used_size > (total_size-1))
+        {
+            cout<<"Array out of bounds\n";
+            return;
+        }
+        used_size++;
+        T temp = arr[pos];
+        arr[pos] = ele;
+        for (int i = (pos+1); i < used_size; i++)
+        {
+            arr[i] = temp;
+            temp = arr[i+1]; 
+        }
+        arr[used_size] = temp;
+        // used_size++;
+    }
+
+    void remove_at(int pos){
+        if(pos < 0){return;}
+        T temp;
+        for (int i = pos; i < (used_size-1); i++)
+        {
+            temp = arr[i+1];
+            arr[i] = temp;
+        }
+        used_size--;
     }
 };
 
 int main()
 {
-    {
-        Array<double> a(100,5);
-    }
-    Array<int> m(10,4);
+    Array<int> m(10, 4);
     m[0] = 1;
     m[1] = 2;
     m[2] = 3;
     m[3] = 4;
-    
+    m.show();
+    m.remove_at(1);
+    m.show();
+
     return 0;
 }
